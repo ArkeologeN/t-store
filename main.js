e50ef7ef225a065a837708721df666d69defe09e
ArkeologeN <hamzawaqas.programmer@gmail.com>
2013-11-11 03:33:40 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,359 @@
+/**
+ * Notes:
+ * - Coordinates are specified as (X, Y, Z) where X and Z are horizontal and Y
+ *   is vertical
+ */
+
+// 12 walls are hard-coded based upon no. of categories via API. (for now)
+var map = [ // 1  2  3  4  5  6  7  8  9
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 0
+           [1, 1, 0, 0, 0, 0, 0, 1, 1, 1,], // 1
+           [1, 1, 0, 0, 2, 0, 0, 0, 0, 1,], // 2
+           [1, 0, 0, 0, 0, 2, 0, 0, 0, 1,], // 3
+           [1, 0, 0, 2, 0, 0, 2, 0, 0, 1,], // 4
+           [1, 0, 0, 0, 2, 0, 0, 0, 1, 1,], // 5
+           [1, 1, 1, 0, 0, 0, 0, 1, 1, 1,], // 6
+           [1, 1, 1, 0, 0, 1, 0, 0, 1, 1,], // 7
+           [1, 1, 1, 1, 1, 1, 0, 0, 1, 1,], // 8
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 9
+           [1, 1, 1, 0, 1, 1, 1, 1, 1, 1,], // 10
+           [1, 1, 1, 1, 1, 1, 0, 1, 1, 0,], // 11
+           ], mapW = map.length, mapH = map[0].length;
+
+// Semi-constants
+var WIDTH = window.innerWidth,
+	HEIGHT = window.innerHeight,
+	ASPECT = WIDTH / HEIGHT,
+	UNITSIZE = 250,
+	WALLHEIGHT = UNITSIZE / 3,
+	MOVESPEED = 100,
+	LOOKSPEED = 0.075,
+	BULLETMOVESPEED = MOVESPEED * 5,
+	NUMAI = 5,
+	PROJECTILEDAMAGE = 20
+	, categories, category;
+// Global vars
+var t = THREE, scene, cam, renderer, controls, clock, projector, model, skin;
+var runAnim = true, mouse = { x: 0, y: 0 }, walls = [];
+/*
+var finder = new PF.AStarFinder({ // Defaults to Manhattan heuristic
+	allowDiagonal: true,
+}), grid = new PF.Grid(mapW, mapH, map);
+*/
+
+// Initialize and run on document ready
+$(document).ready(function() {
+	$('body').append('<div id="intro">Welcome to MySMA Mart!</div>');
+	$('#intro').css({width: WIDTH, height: HEIGHT}).one('click', function(e) {
+		e.preventDefault();
+		$(this).fadeOut();
+		init();
+		
+		animate();
+	});
+	/*
+	new t.ColladaLoader().load('models/Yoshi/Yoshi.dae', function(collada) {
+		model = collada.scene;
+		skin = collada.skins[0];
+		model.scale.set(0.2, 0.2, 0.2);
+		model.position.set(0, 5, 0);
+		scene.add(model);
+	});
+	*/
+});
+
+// Setup
+function init() {
+	clock = new t.Clock(); // Used in render() for controls.update()
+	projector = new t.Projector(); // Used in bullet projection
+	scene = new t.Scene(); // Holds all objects in the canvas
+	scene.fog = new t.FogExp2(0xD6F1FF, 0.0005); // color, density
+	
+	// Set up camera
+	cam = new t.PerspectiveCamera(60, ASPECT, 1, 10000); // FOV, aspect, near, far
+	cam.position.y = UNITSIZE * .2;
+	scene.add(cam);
+	
+	// Camera moves with mouse, flies around with WASD/arrow keys
+	controls = new t.FirstPersonControls(cam);
+	controls.movementSpeed = MOVESPEED;
+	controls.lookSpeed = LOOKSPEED;
+	controls.lookVertical = false; // Temporary solution; play on flat surfaces only
+	controls.noFly = true;
+
+	// World objects
+	setupScene();
+	
+	// Artificial Intelligence
+	//setupAI();
+	
+	// Handle drawing as WebGL (faster than Canvas but less supported)
+	renderer = new t.WebGLRenderer();
+	renderer.setSize(WIDTH, HEIGHT);
+	
+	// Add the canvas to the document
+	renderer.domElement.style.backgroundColor = '#D6F1FF'; // easier to see
+	document.body.appendChild(renderer.domElement);
+	
+	// Track mouse position so we know where to shoot
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	
+	
+	// Display HUD
+	//$('body').append('<canvas id="radar" width="200" height="200"></canvas>');
+	//$('body').append('<div id="hud"><p>Health: <span id="health">100</span><br />Score: <span id="score">0</span></p></div>');
+	$('body').append('<div id="credits"><p>Developed at <a href="http://www.gnstudio.com/">GNStudio</a> by <a href="http://github.com/ArkeologeN">Hamza Waqas</a></p></div>');
+	
+	// Set up "hurt" flash
+	$('body').append('<div id="hurt"></div>');
+	$('#hurt').css({width: WIDTH, height: HEIGHT,});
+}
+
+// Helper function for browser frames
+function animate() {
+	if (runAnim) {
+		requestAnimationFrame(animate);
+	}
+	render();
+}
+
+// Update and display
+function render() {
+	var delta = clock.getDelta(), speed = delta * BULLETMOVESPEED;
+	var aispeed = delta * MOVESPEED;
+	controls.update(delta); // Move camera
+	
+
+	// Update AI.
+	for (var i = ai.length-1; i >= 0; i--) {
+		var a = ai[i];
+		// Move AI
+		var r = Math.random();
+		if (r > 0.995) {
+			a.lastRandomX = Math.random() * 2 - 1;
+			a.lastRandomZ = Math.random() * 2 - 1;
+		}
+		a.translateX(aispeed * a.lastRandomX);
+		a.translateZ(aispeed * a.lastRandomZ);
+		var c = getMapSector(a.position);
+		if (c.x < 0 || c.x >= mapW || c.y < 0 || c.y >= mapH || checkWallCollision(a.position)) {
+			a.translateX(-2 * aispeed * a.lastRandomX);
+			a.translateZ(-2 * aispeed * a.lastRandomZ);
+			a.lastRandomX = Math.random() * 2 - 1;
+			a.lastRandomZ = Math.random() * 2 - 1;
+		}
+		
+		/*
+		var c = getMapSector(a.position);
+		if (a.pathPos == a.path.length-1) {
+			console.log('finding new path for '+c.x+','+c.z);
+			a.pathPos = 1;
+			a.path = getAIpath(a);
+		}
+		var dest = a.path[a.pathPos], proportion = (c.z-dest[1])/(c.x-dest[0]);
+		a.translateX(aispeed * proportion);
+		a.translateZ(aispeed * 1-proportion);
+		console.log(c.x, c.z, dest[0], dest[1]);
+		if (c.x == dest[0] && c.z == dest[1]) {
+			console.log(c.x+','+c.z+' reached destination');
+			a.PathPos++;
+		}
+		*/
+		var cc = getMapSector(cam.position);
+		if (Date.now() > a.lastShot + 750 && distance(c.x, c.z, cc.x, cc.z) < 2) {
+			createBullet(a);
+			a.lastShot = Date.now();
+		}
+	}
+
+	renderer.render(scene, cam); // Repaint
+	
+	
+}
+
+// Set up the objects in the world
+function setupScene() {
+	var UNITSIZE = 250, units = mapW;
+	
+	/*
+	 * var roof = new t.Mesh(
+			new t.CubeGeometry(units / UNITSIZE, 10, units / UNITSIZE),
+			new t.MeshLambertMaterial({map: t.ImageUtils.loadTexture('images/textures/skybox/roof-tile-1.jpg')}));
+			
+	scene.add(roof);
+	*/
+
+	// Geometry: floor
+	var floor = new t.Mesh(
+			new t.CubeGeometry(units * UNITSIZE, 10, units * UNITSIZE),
+			new t.MeshLambertMaterial({map: t.ImageUtils.loadTexture('images/floor-3.jpg')})
+	);
+	//color: 0xEDCBA0,
+	scene.add(floor);
+	
+	// Geometry: walls
+	var cube = new t.CubeGeometry(UNITSIZE, WALLHEIGHT, UNITSIZE);
+	var materials = [
+		 new t.MeshLambertMaterial({/*color: 0x00CCAA,*/map: t.ImageUtils.loadTexture('images/shelf.png')}),
+		 new t.MeshLambertMaterial({/*color: 0xC5EDA0,*/map: t.ImageUtils.loadTexture('images/shelf-2.png')}),
+		 new t.MeshLambertMaterial({color: 0xFBEBCD}),
+	 ];
+	 
+	 APIRequest.loadCategories(function(data) {
+			categories = $.parseJSON(data);
+			if ( categories.success === true) {
+				for (var i = 0; i < categories.categories.length; i++) {
+					for (var j = 0, m = map[i].length; j < m; j++) {
+						if (map[i][j]) {
+							var wall = new t.Mesh(cube, materials[map[i][j]-1]);
+							wall.position.x = (i - units/2) * UNITSIZE;
+							wall.position.y = WALLHEIGHT/2;
+							wall.position.z = (j - units/2) * UNITSIZE;
+							console.log(typeof wall);
+							scene.add(wall);
+						}
+					}
+				}
+			}
+	 });
+	 
+	
+	
+	
+	
+	// Lighting
+	var directionalLight1 = new t.DirectionalLight( 0xF7EFBE, 0.7 );
+	directionalLight1.position.set( 0.5, 1, 0.5 );
+	scene.add( directionalLight1 );
+	var directionalLight2 = new t.DirectionalLight( 0xF7EFBE, 0.8 );
+	directionalLight2.position.set( -0.5, -1, -0.5 );
+	scene.add( directionalLight2 );
+}
+
+var ai = [];
+var aiGeo = new t.CubeGeometry(40, 40, 40);
+function setupAI() {
+	for (var i = 0; i < NUMAI; i++) {
+//addAI();
+	}
+}
+
+
+function getAIpath(a) {
+	var p = getMapSector(a.position);
+	do { // Cop-out
+		do {
+			var x = getRandBetween(0, mapW-1);
+			var z = getRandBetween(0, mapH-1);
+		} while (map[x][z] > 0 || distance(p.x, p.z, x, z) < 3);
+		var path = findAIpath(p.x, p.z, x, z);
+	} while (path.length == 0);
+	return path;
+}
+
+/**
+ * Find a path from one grid cell to another.
+ *
+ * @param sX
+ *   Starting grid x-coordinate.
+ * @param sZ
+ *   Starting grid z-coordinate.
+ * @param eX
+ *   Ending grid x-coordinate.
+ * @param eZ
+ *   Ending grid z-coordinate.
+ * @returns
+ *   An array of coordinates including the start and end positions representing
+ *   the path from the starting cell to the ending cell.
+ */
+function findAIpath(sX, sZ, eX, eZ) {
+	var backupGrid = grid.clone();
+	var path = finder.findPath(sX, sZ, eX, eZ, grid);
+	grid = backupGrid;
+	return path;
+}
+
+function distance(x1, y1, x2, y2) {
+	return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+
+function getMapSector(v) {
+	var x = Math.floor((v.x + UNITSIZE / 2) / UNITSIZE + mapW/2);
+	var z = Math.floor((v.z + UNITSIZE / 2) / UNITSIZE + mapW/2);
+	return {x: x, z: z};
+}
+
+/**
+ * Check whether a Vector3 overlaps with a wall.
+ *
+ * @param v
+ *   A THREE.Vector3 object representing a point in space.
+ *   Passing cam.position is especially useful.
+ * @returns {Boolean}
+ *   true if the vector is inside a wall; false otherwise.
+ */
+function checkWallCollision(v) {
+	var c = getMapSector(v);
+	return map[c.x][c.z] > 0;
+}
+
+
+var bullets = [];
+var sphereMaterial = new t.MeshBasicMaterial({color: 0x333333});
+var sphereGeo = new t.SphereGeometry(2, 6, 6);
+
+/*
+function loadImage(path) {
+	var image = document.createElement('img');
+	var texture = new t.Texture(image, t.UVMapping);
+	image.onload = function() { texture.needsUpdate = true; };
+	image.src = path;
+	return texture;
+}
+*/
+
+function onDocumentMouseMove(e) {
+	e.preventDefault();
+	mouse.x = (e.clientX / WIDTH) * 2 - 1;
+	mouse.y = - (e.clientY / HEIGHT) * 2 + 1;
+}
+
+// Handle window resizing
+$(window).resize(function() {
+	WIDTH = window.innerWidth;
+	HEIGHT = window.innerHeight;
+	ASPECT = WIDTH / HEIGHT;
+	if (cam) {
+		cam.aspect = ASPECT;
+		cam.updateProjectionMatrix();
+	}
+	if (renderer) {
+		renderer.setSize(WIDTH, HEIGHT);
+	}
+	$('#intro, #hurt').css({width: WIDTH, height: HEIGHT,});
+});
+
+// Stop moving around when the window is unfocused (keeps my sanity!)
+$(window).focus(function() {
+	if (controls) controls.freeze = false;
+});
+$(window).blur(function() {
+	if (controls) controls.freeze = true;
+});
+
+//Get a random integer between lo and hi, inclusive.
+//Assumes lo and hi are integers and lo is lower than hi.
+function getRandBetween(lo, hi) {
+ return parseInt(Math.floor(Math.random()*(hi-lo+1))+lo, 10);
+}
+
+var APIRequest = {}
+APIRequest.loadCategories =  function(cb) {
+	$.ajax({
+		url: "http://mysma.gnstudio.biz/index.php?route=feed/web_api/categories&parent=0&level=2",
+		success: function(data) {
+			cb(data);
+		}
+	});
+}
+
